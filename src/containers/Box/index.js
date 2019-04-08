797d1536e43f228655ea32543229cae7096e01bc
Eduard Grabina <eduardgrabina@gmail.com>
2019-04-08 23:17:54 +0300

    Добавил столкновение двух объектов

--- src/containers/Box/index.js
+++ src/containers/Box/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { cloneDeep } from "lodash";
 
 import "./style.scss";
 
@@ -6,32 +7,65 @@ class Box extends Component {
   constructor(props) {
     super(props);
 
-    this.moveElement = this.moveElement.bind(this);
+    this.onMoveElement = this.onMoveElement.bind(this);
+    this.collision = this.collision.bind(this);
 
-    this.state = {};
+    this.state = {
+      container: {
+        width: 300,
+        height: 300,
+      },
+      player: {
+        width: 30,
+        height: 30,
+        x: 0,
+        y: 0,
+      },
+      obj: {
+        width: 15,
+        height: 15,
+        x: 60,
+        y: 60,
+      },
+    };
   }
 
   componentDidMount() {
-    document.body.addEventListener("keydown", this.moveElement);
+    document.body.addEventListener("keydown", this.onMoveElement);
   }
 
-  moveElement(e) {
-    e.preventDefault();
-    let block = this.block;
-    let container = this.container;
+  componentWillUnmount() {
+    document.body.removeEventListener("keydown", this.onMoveElement);
+  }
 
-    let blockWidth = block.clientWidth;
-    let blockHeight = block.clientHeight;
-    let containerWidth = container.clientWidth;
-    let containerHeight = container.clientHeight;
-    let playZone = {
-      width: containerWidth - blockWidth,
-      height: containerHeight - blockHeight,
+  collision(obj1, obj2) {
+    let Xcoll = false;
+    let Ycoll = false;
+
+    if (obj1.x + obj1.width >= obj2.x && obj1.x <= obj2.x + obj2.width)
+      Xcoll = true;
+    if (obj1.y + obj1.height >= obj2.y && obj1.y <= obj2.y + obj2.height)
+      Ycoll = true;
+
+    if (Xcoll && Ycoll) {
+      return true;
+    }
+    return false;
+  }
+
+  onMoveElement(e) {
+    e.preventDefault();
+
+    let player = cloneDeep(this.state.player);
+
+    const playZone = {
+      width: this.state.container.width - player.width,
+      height: this.state.container.height - player.height,
     };
 
-    let left = Number(block.style.left.slice(0, -2));
-    let top = Number(block.style.top.slice(0, -2));
-    let step = 20;
+    let left = player.x;
+    let top = player.y;
+    const step = 20;
 
     switch (e.keyCode) {
       case 37:
@@ -50,37 +84,70 @@ class Box extends Component {
         break;
     }
 
-    if (left > 0 && left < playZone.width) {
-      block.style.left = left + "px";
-    } else if (left >= playZone.width) {
-      block.style.left = playZone.width + "px";
-    } else if (left <= playZone.width) {
-      block.style.left = 0 + "px";
+    if (left > playZone.width) {
+      left = playZone.width - 2;
+    } else if (left < 0) {
+      left = 0;
     }
 
-    if (top > 0 && top < playZone.height) {
-      block.style.top = top + "px";
-    } else if (top >= playZone.height) {
-      block.style.top = playZone.height + "px";
-    } else if (top <= playZone.height) {
-      block.style.top = 0 + "px";
+    if (top > playZone.height) {
+      top = playZone.height - 2;
+    } else if (top < 0) {
+      top = 0;
     }
+
+    player.x = left;
+    player.y = top;
+
+    let boom = this.collision(player, this.state.obj);
+
+    if (boom) {
+      console.log("boom!");
+    }
+
+    this.setState({ player });
   }
 
   render() {
+    const container = this.state.container;
+    const player = this.state.player;
+    const obj = this.state.obj;
+
+    const containerStyle = {
+      width: container.width,
+      height: container.height,
+    };
+
+    const playerStyle = {
+      width: player.width,
+      height: player.height,
+      top: player.y,
+      left: player.x,
+    };
+
+    const enemyStyle = {
+      width: obj.width,
+      height: obj.height,
+      top: obj.y,
+      left: obj.x,
+    };
+
     return (
       <div
         className={"box__container"}
+        style={containerStyle}
         ref={container => {
           this.container = container;
         }}>
         <div
           className={"box__element"}
+          style={playerStyle}
           ref={block => {
             this.block = block;
           }}
-          onKeyPress={this.moveElement}
+          onKeyPress={this.onMoveElement}
         />
+        <div className={"box__enemy"} style={enemyStyle} />
       </div>
     );
   }
